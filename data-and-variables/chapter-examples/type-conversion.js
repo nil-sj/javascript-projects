11f92ec30d10131d64daade2e29dae4448fa5cc6
Nil SJ <niladri123@gmail.com>
2024-01-10 18:14:40 -0500

    Completed Class2 Exercises - Booleans

--- data-and-variables/chapter-examples/type-conversion.js
+++ data-and-variables/chapter-examples/type-conversion.js
@@ -7,3 +7,8 @@ console.log(Number("23bottles"));
 console.log(String(17));
 console.log(String(123.45));
 console.log(typeof String(123.45));
+
+let myString = "12.345";
+console.log("myString = ", myString, " and typeof myString is ", typeof myString);
+let myNum = Number(myString);
+console.log("myNum = ", myNum, " and typeof myNum is ", typeof myNum);
