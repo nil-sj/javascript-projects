8029d2425e93f141e96ef98534364a6a54fb02f5
Nil SJ <niladri123@gmail.com>
2024-01-18 22:08:59 -0500

    Completed Class 3 Studio - on Arrays and Strings

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -4,11 +4,34 @@ let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let foodCabinet = food.split(',');
+let equipmentCabinet = equipment.split(',');
+let petsCabinet = pets.split(',');
+let sleepAidsCabinet = sleepAids.split(',');
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [foodCabinet, equipmentCabinet, petsCabinet, sleepAidsCabinet];
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+userInput = Number(input.question("Select a cabinet (0-3): "));
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
-
+if (userInput < 0 || userInput >= cargoHold.length) {
+    console.log(`You have entered ${userInput} - which is invalid. It must be betweeen 0 and ${cargoHold.length - 1}.`);
+} else {
+    console.log(`The contents of the selected cabinet at index '${userInput}' are: ${cargoHold[userInput]}.`);
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+const newUserInputNum = Number(input.question("Select a cabinet (0-3): "));
+const userInputItem = input.question("Enter an item to be checked: ");
+if (newUserInputNum < 0 || newUserInputNum >= cargoHold.length) {
+    console.log(`You have entered ${newUserInputNum} - which is invalid. It must be betweeen 0 and ${cargoHold.length - 1}.`);
+} else {
+    if (cargoHold[newUserInputNum].includes(userInputItem)) {
+        console.log(`Yes, the cabinet contains: ${cargoHold[newUserInputNum]}. So, the item ${userInputItem} was found in it.`);
+    } else {
+        console.log(`No, the cabinet contains: ${cargoHold[newUserInputNum]}. So, I am afraid, the item ${userInputItem} was not found in it.`);
+    }
+}
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,24 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let newString = str.slice(3).concat(str.slice(0, 0+3));
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The original string was "${str}", and the new string is "${newString}".`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let numChars = Number(input.question("Enter the number of letters to be relocated: "));
+let newUserString = str.slice(numChars).concat(str.slice(0, 0+numChars));
+console.log(`The original string was "${str}"\nThe user wanted to relocate ${numChars} characters.\nSo, the new string is "${newUserString}".`);
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+let validatedNumChars = -1;
+while (validatedNumChars < 0) {
+    validatedNumChars = Number(input.question(`Enter a non-negative number i.e., the number of letters to be relocated (valid numbers are between 0 and ${str.length}): `));
+    if (validatedNumChars > str.length) {
+        console.log(`Since you entered ${validatedNumChars} which is greater than the length of the original string, we consider default of 3`);
+        validatedNumChars = 3;        
+    } 
+}
+let newValidatedUserString = str.slice(validatedNumChars).concat(str.slice(0, 0+validatedNumChars));
+console.log(`The original string was "${str}"\nThe user wanted to relocate ${validatedNumChars} - validated characters.\nSo, the new string is "${newValidatedUserString}".`);
