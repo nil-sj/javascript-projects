a6034d63886c3c111f312646db610f15250e9ec7
Nil SJ <niladri123@gmail.com>
2024-01-20 19:42:25 -0500

    Completed Loops Exercises

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -3,22 +3,67 @@
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
-
-
+console.log("\na. Print the numbers 0 - 20, one number per line.");
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
+}
+console.log("\nb. Print only the ODD values from 3 - 29, one number per line.");
+for (let i = 3; i <= 29; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  } 
+}
+console.log("\nc. Print the EVEN numbers 12 to -14 in descending order, one number per line.");
+for (let i = 12; i >= -14; i--) {
+  if (i % 2 === 0) {
+    console.log(i);
+  } 
+}
+console.log("\nd. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers).");
+for (let i = 50; i >= 20; i--) {
+  if (i % 3 === 0) {
+    console.log(i);
+  } 
+}
 
 /*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
+let myString = "LaunchCode";
+let myArray = [1, 5, 'LC101', 'blue', 42];
 
-
-Construct ``for`` loops to accomplish the following tasks:
+/*Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+console.log("\na. Print each element of the array to a new line.");
 
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
 
+console.log("\nb. Print each character of the string - in reverse order - to a new line.");
 
-
+for (let i = myString.length-1; i >= 0; i--) {
+  console.log(myString[i]);
+}
 
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+
+let originalArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+
+console.log("\nExercise #3");
+
+let oddArray = [];
+let evenArray = [];
+
+for (let i = 0; i < originalArray.length; i++) {
+  if (originalArray[i] % 2 === 0) {
+    evenArray.push(originalArray[i]);
+  } else {
+    oddArray.push(originalArray[i]);
+  }
+}
+
+console.log(`The even array is: [${evenArray}]`);
+console.log(`The odd array is: [${oddArray}]`);
--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,25 +1,60 @@
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
+const input = require('readline-sync');
 
-
-
+let initialFuelLevel = 0;
+let numAtronautsAboard = 0;
+let currentAltitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
 
-
-
-
+while (initialFuelLevel <= 5000 || initialFuelLevel >= 30000 || isNaN(initialFuelLevel)) {
+  if (isNaN(initialFuelLevel)) {
+    initialFuelLevel = Number(input.question("Sorry, it must be a number. Enter the initial fuel level again: "));
+  } else if (!initialFuelLevel) {
+    initialFuelLevel = Number(input.question("Enter the initial fuel level (a number): "));
+  } else if (initialFuelLevel <= 5000) {
+    initialFuelLevel = Number(input.question("Sorry, it must be greater than 5000. Enter the initial fuel level again: "));
+  } else if (initialFuelLevel >= 30000) {
+    initialFuelLevel = Number(input.question("Sorry, it must be less than 30000. Enter the initial fuel level again: "));
+  }
+}
 
 //b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
-  
-  
-  
+
+while (numAtronautsAboard < 1 || numAtronautsAboard > 7 || isNaN(numAtronautsAboard)) {
+  if (isNaN(numAtronautsAboard)) {
+    numAtronautsAboard = Number(input.question("Sorry, it must be a number (an integer). Enter the number of astronauts again: "));
+  } else if(!numAtronautsAboard) {
+    numAtronautsAboard = Number(input.question("Enter the number of atronauts aboard (an integer): "));
+  } else if (numAtronautsAboard < 1) {
+    numAtronautsAboard = Number(input.question("Sorry, it must be at least 1. Enter the number of astronauts again: "));
+  } else if (numAtronautsAboard > 7) {
+    numAtronautsAboard = Number(input.question("Sorry, it must not be more than 7. Enter the number of astronauts again: "));
+  } 
+}
   
 //c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
 
+console.log("\nMonitoring the fuel status and altitude of the shuttle...");
 
+let currentFuelLevel = initialFuelLevel;
+
+while (currentFuelLevel > 100*numAtronautsAboard) {
+  currentAltitude += 50;
+  currentFuelLevel -= 100*numAtronautsAboard;
+  console.log(`*** Current Altitude: ${currentAltitude} km | Current Fuel Level: ${currentFuelLevel}`);
+}
 
 /*Exercise #5: Output the result with the phrase, “The shuttle gained an altitude of ___ km.”
-
 If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”*/
+
+console.log("Displaying final status: -------------------");
+let displayText = `The shuttle gained an altitude of ${currentAltitude} km.`;
+if (currentAltitude >= 2000) {
+  displayText += " Orbit achieved!";
+} else {
+  displayText += " Failed to reach orbit";
+}
+console.log(displayText);
