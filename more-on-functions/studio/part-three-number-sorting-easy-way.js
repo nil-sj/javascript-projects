d879d0befa19f6e667160c4176d4247ef8ee3aaa
Nil SJ <niladri123@gmail.com>
2024-01-29 20:19:33 -0500

    Completed Studio 6 - More on Functions

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -5,4 +5,16 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
 
+console.log(`Array nums1 before sorting: [${nums1}]`);
+console.log(nums1.sort(function (a, b) { return a - b }));
+console.log(`Array nums1 after sorting: [${nums1}]`);
+// the above checks whether the function above alters the original array or not.
+
+console.log(nums2.sort(function (a, b) { return a - b }));
+console.log(nums3.sort(function (a, b) { return a - b }));
+
 //Sort each array in decending order.
+
+console.log(nums1.sort(function (a, b) { return b - a }));
+console.log(nums2.sort(function (a, b) { return b - a }));
+console.log(nums3.sort(function (a, b) { return b - a }));
--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,7 +1,7 @@
-function findMinValue(arr){
+function findMinValue(arr) {
   let min = arr[0];
-  for (i = 0; i < arr.length; i++){
-    if (arr[i] < min){
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
       min = arr[i];
     }
   }
@@ -20,10 +20,36 @@ function findMinValue(arr){
 
 //Your function here...
 
-/* BONUS MISSION: Refactor your sorting function to use recursion below:
- */
+function sortNumArray(numArr) {
+  let sortedNumArr = [];
+  while (numArr.length > 0) {
+    sortedNumArr.push(findMinValue(numArr));
+    numArr.splice(numArr.indexOf(findMinValue(numArr)), 1);
+  }
+  return sortedNumArr;
+}
+
+/* BONUS MISSION: Refactor your sorting function to use recursion below: */
+
+function recursiveSortNumArray(numArr) {
+  if (numArr.length < 2) {
+    return numArr;
+  } else {
+    let minValue = findMinValue(numArr);
+    numArr.splice(numArr.indexOf(minValue), 1)
+    return [minValue].concat(recursiveSortNumArray(numArr));
+  }
+}
 
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+console.log(`The nums1 array at the start is [${nums1}]`);
+console.log(sortNumArray(nums1));
+console.log(`After applying the sortNumArray function, nums1 array looks like: [${nums1}]`);
+
+console.log(`The nums3 array at the start is [${nums3}]`);
+console.log(recursiveSortNumArray(nums3));
+console.log(`After applying the recursiveSortNumArray function, nums3 array looks like: [${nums3}]`);
