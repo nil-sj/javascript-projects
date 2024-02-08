7440629bb18e8aed9b1bca96dc55483c86bc04d4
Nil SJ <niladri123@gmail.com>
2024-02-08 13:20:57 -0500

    Completed Class 9 Studio - Unit Testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,52 @@
-
 let launchcode = {
-  
-}
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(n){
+        // if(n%2 === 0 && n%3 === 0 && n%5 === 0) {
+        //     return 'LaunchCode Rocks!'; 
+        // }
+        // if(n%2 === 0 && n%3 === 0) {
+        //     return 'LaunchCode!'; 
+        // }
+        // if(n%3 === 0 && n%5 === 0) {
+        //     return 'Code Rocks!'; 
+        // }
+        // if(n%2 === 0 && n%5 === 0) {
+        //     return 'Launch Rocks! (CRASH!!!!)'; 
+        // }
+        // if(n%2 === 0) {
+        //     return 'Launch!'; 
+        // }
+        // if(n%3 === 0) {
+        //     return 'Code!'; 
+        // }
+        // if(n%5 === 0) {
+        //     return 'Rocks!'; 
+        // }
+        // if(n%2 !== 0 && n%3 !== 0 && n%5 !== 0) {
+        //     return 'Rutabagas! That doesn\'t work.'; 
+        // }
+
+        // refactoring for bonus mission
+        const logicObj = {
+            2: "Launch",
+            3: "Code",
+            5: "Rocks"
+        };
+        let outputString = "";
+        for (let divisor in logicObj) {
+            if (n % divisor == 0) {
+                (divisor == 5 && outputString) && (outputString += " ");
+                outputString += logicObj[divisor];
+            }
+        }
+        outputString += outputString ? "!" : "Rutabagas! That doesn't work.";
+        (outputString === 'Launch Rocks!') && (outputString += ' (CRASH!!!!)');
+        return outputString;
+    } 
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,44 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test ("should have a property 'organization' with a value 'nonprofit'", function(){
+    expect(launchcode.organization).toBe('nonprofit');
+  });
+  test ("should have a property 'executiveDirector' with a value 'Jeff'", function(){
+    expect(launchcode.executiveDirector).toBe('Jeff');
+  });
+  test ("should have a property 'percentageCoolEmployees' with a value 100", function(){
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test ("should have a property 'programsOffered' with an array that holds 3 programs: 'Web Development', 'Data Analysis', and 'Liftoff'", function(){
+    expect(launchcode.programsOffered[0]).toBe('Web Development');
+    expect(launchcode.programsOffered[1]).toBe('Data Analysis');
+    expect(launchcode.programsOffered[2]).toBe('Liftoff');
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test ("should have a method that returns 'Launch!' when input num is divisible by only 2", function(){
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+  });
+  test ("should have a method that returns 'Code!' when input num is divisible by only 3", function(){
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+  });
+  test ("should have a method that returns 'Rocks!' when input num is divisible by only 5", function(){
+    expect(launchcode.launchOutput(5)).toBe('Rocks!');
+  });
+  test ("should have a method that returns 'LaunchCode!' when input num is divisible by 2 AND 3", function(){
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+  });
+  test ("should have a method that returns 'Code Rocks!' when input num is divisible by 3 AND 5", function(){
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+  });
+  test ("should have a method that returns 'Launch Rocks! (CRASH!!!!)' when input num is divisible by 2 AND 5", function(){
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+  });
+  test ("should have a method that returns 'LaunchCode Rocks!' when input num is divisible by 2, 3, AND 5", function(){
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+  });
+  test ("should have a method that returns 'Rutabagas! That doesn't work.' when input num is NOT divisible by 2, 3, or 5", function(){
+    expect(launchcode.launchOutput(11)).toBe("Rutabagas! That doesn't work.");
+  });
+
 });
