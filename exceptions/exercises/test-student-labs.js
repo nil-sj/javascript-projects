e6f4d904d1a8aef60cd150cef42fbb6c299dce98
Nil SJ <niladri123@gmail.com>
2024-02-11 19:16:17 -0500

    Completed Class 10 Exercises - Exceptions

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,16 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    try {
+      if(!lab.runLab) {
+        throw Error('The runLab method does not exist!')
+      } else {
+        let result = lab.runLab(3);
+        console.log(`${lab.student} code worked: ${result === 27}`);
+      }
+    } catch(error) {
+      console.log(`${lab.student} code has an error: ${error.message}`);
+    }
   }
 }
 
@@ -22,3 +30,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
