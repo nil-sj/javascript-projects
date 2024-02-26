4608b187ffe12ed73675f2d0b3aa3d32123ede05
Nil SJ <niladri123@gmail.com>
2024-02-26 17:40:37 -0500

    Completed Class 14 Exercises -  DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,21 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+    missionAbort.addEventListener('mouseover', event => {
+        missionAbort.style.backgroundColor = 'red';
+    });
+    missionAbort.addEventListener('mouseout', event => {
+        missionAbort.style.backgroundColor = 'unset';
+    });
+    missionAbort.addEventListener('click', event => {
+        let confirmation = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmation) {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    });
 }
 
 window.addEventListener("load", init);
