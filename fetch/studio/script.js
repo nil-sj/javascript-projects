1286c08a2360d0e1c7c8400403185f40ec9cb949
Nil SJ <niladri123@gmail.com>
2024-03-04 21:08:51 -0500

    Completed Class 16 Studio: Fetch and JSON

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,39 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+    const container = document.querySelector('#container');
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function(response){
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then(response => {
+
+        let sortedResponse = response.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+
+        sortedResponse.forEach(item => {
+
+            let skills = '';
+            item.skills.forEach(skill => {
+                skills += skills ? ', ' : '';
+                skills += skill;
+            });
+
+            container.innerHTML += `
+                <div class="astronaut" data-hours="${item.hoursInSpace}">
+                    <div class="bio">
+                        <h3>${item.firstName} ${item.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${item.hoursInSpace}</li>
+                            <li class="${item.active ? 'green' : ''}">Active: ${item.active}</li>
+                            <li>Skills: ${skills}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${item.picture}">
+                </div>`;
+
+        });
+        container.innerHTML += `<br><p>Astronaut Count: ${response.length}</p>`;
+    });
+});
+
